d9bf0e94f95bf111a4a8b47043ec294c03ff2937
Ivan Linnik <Linnik.I1@citilink.ru>
2024-04-13 14:20:35 +0300

    Finished chapter 9
    Learned streaming render, used loading-file, Suspense, take down
    fetching data on current component level

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,10 +1,13 @@
+import { lusitana } from '@/app/ui/fonts';
+
 import {
   BanknotesIcon,
   ClockIcon,
   UserGroupIcon,
   InboxIcon,
 } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,18 +17,26 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  } = await fetchCardData();
+
   return (
     <>
-      {/* NOTE: comment in this code when you get to this point in the course */}
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
+
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
@@ -45,6 +56,7 @@ export function Card({
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
       <div className="flex p-4">
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
+
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
